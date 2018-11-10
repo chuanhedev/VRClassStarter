d190710c53115da0dd18223577a5137d35745699
cx <chixuchixu@hotmail.com>
2018-11-10 17:57:45 +0800

    handle server.zip

--- VRClassStarter/Form1.cs
+++ VRClassStarter/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Net;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using Newtonsoft.Json.Linq;
 using ICSharpCode.SharpZipLib.Zip;
 using ICSharpCode.SharpZipLib.Core;
@@ -29,9 +30,11 @@ namespace VRClassUpdater
         JArray fileInfos;
         int fileIndex = 0;
         bool updateClient = false;
+        bool updateServer = false;
         string teacherFileName = "teacher.zip";
         string serverFileName = "server.zip";
         string localConfigFileName = "config.json";
+        string versionNumber;
 
         public Form1()
         {
@@ -55,6 +58,8 @@ namespace VRClassUpdater
                 return;
             }
             //JObject sv = JObject.Parse(serverVersion);
+            Debug.WriteLine("localVersion " + localVersion["version"]);
+            Debug.WriteLine("serverVersion " + serverVersion["version"]);
             fileInfos = (JArray)serverVersion["files"];
             Debug.WriteLine(fileInfos.Count);
             if (!Directory.Exists(tempFolderPath))
@@ -62,17 +67,25 @@ namespace VRClassUpdater
                 Directory.CreateDirectory(tempFolderPath);
             }
 
-            // updateClient = serverVersion["update"].ToString() == "1";
-            // if (updateClient)
-            // {
-            //     files.Add(teacherFileName);
-            // }
-               
-            // for(int i=0; i<fileInfos.Count; i++)
-            // {
-            //     files.Add(fileInfos[i]["name"].ToString());
-            // }
-            
+            updateClient = serverVersion["update_teacher"].ToString() == "1";
+            if (updateClient)
+            {
+                JObject t = new JObject();
+                t["name"] = teacherFileName;
+                t["path"] = "";
+                fileInfos.Add(t);
+            }
+            //KillNodeProcess();
+            updateServer = serverVersion["update_server"].ToString() == "1";
+            if (updateServer)
+            {
+                KillNodeProcess();
+                JObject t = new JObject();
+                t["name"] = serverFileName;
+                t["path"] = "";
+                fileInfos.Add(t);
+            }
+
             if (fileIndex < fileInfos.Count)
             {
                label1.Text = string.Format("下载中({0}/{1})", fileIndex + 1, fileInfos.Count);
@@ -80,66 +93,150 @@ namespace VRClassUpdater
                webClient = new WebClient();
                webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
                webClient.DownloadProgressChanged += Wb_DownloadProgressChanged;
-               webClient.DownloadFileAsync(new Uri(downloadRoot + fileInfo["path"] + fileInfo["name"]), tempFolderPath +  fileInfo["path"] + fileInfo["name"]);
-            Debug.WriteLine("downloading ... " + downloadRoot + fileInfo["path"] + fileInfo["name"] + " " + tempFolderPath +  fileInfo["path"] + fileInfo["name"] );
+                webClient.DownloadFileAsync(new Uri(downloadRoot + fileInfo["path"] + fileInfo["name"]), tempFolderPath +  fileInfo["path"] + fileInfo["name"]);
+                Debug.WriteLine("downloading ... " + downloadRoot + fileInfo["path"] + fileInfo["name"] + " " + tempFolderPath +  fileInfo["path"] + fileInfo["name"] );
             }
 
+        }
+
+        public void KillNodeProcess()
+        {
+            Debug.WriteLine("KillNodeProcess");
+            foreach (var process in Process.GetProcessesByName("node"))
+            {
+                //Debug.WriteLine(process.ProcessName);
+                process.Kill();
+            }
+        }
+
+
+        public static bool UnpackFiles(string file, string dir)
+        {
+            try
+            {
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                ZipInputStream s = new ZipInputStream(File.OpenRead(file));
+                ZipEntry theEntry;
+                while ((theEntry = s.GetNextEntry()) != null)
+                {
+                    string directoryName = Path.GetDirectoryName(theEntry.Name);
+                    string fileName = Path.GetFileName(theEntry.Name);
+                    if (directoryName != String.Empty)
+                    {
+                        Directory.CreateDirectory(dir + directoryName);
+                    }
+                    if (fileName != String.Empty)
+                    {
+                        FileStream streamWriter = File.Create(dir + theEntry.Name);
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                streamWriter.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        streamWriter.Close();
+                    }
+                }
+                s.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void UpdateServerFolder()
+        {
+            Debug.WriteLine("UpdateServerFolder");
+            if (updateServer)
+            {
+                DirectoryInfo di = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "server"));
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    if (dir.Name != "resources") 
+                        dir.Delete(true);
+                }
+                System.IO.Compression.ZipFile.ExtractToDirectory(Path.Combine(Directory.GetCurrentDirectory(), tempFolderPath, serverFileName), Path.Combine(Directory.GetCurrentDirectory(), "server"));
+             
+            }
+        }
+
+        private void UpdateTeacherFolder()
+        {
+            Debug.WriteLine("UpdateTeacherFolder");
+            if (updateClient)
+            {
+                DirectoryInfo di = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "client"));
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    if (dir.Name != "") ;
+                    dir.Delete(true);
+                }
+                System.IO.Compression.ZipFile.ExtractToDirectory(Path.Combine(Directory.GetCurrentDirectory(), tempFolderPath, teacherFileName), Path.Combine(Directory.GetCurrentDirectory(), "client"));
+                //UnpackFiles(Path.Combine(Directory.GetCurrentDirectory(), tempFolderPath, teacherFileName), Path.Combine(Directory.GetCurrentDirectory(), "client"));
+                Debug.WriteLine("done");
+            }
         }
 
         private void DownloadComplete()
         {
             Debug.WriteLine("DownloadComplete");
-            // if (updateClient)
-            // {
-            //     DirectoryInfo di = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "client"));
-            //     foreach(FileInfo file in di.GetFiles())
-            //     {
-            //         file.Delete();
-            //     }
-            //     foreach (DirectoryInfo dir in di.GetDirectories())
-            //     {
-            //         dir.Delete(true);
-            //     }
-            //     Debug.WriteLine("DownloadComplete");
-            //     FastZipEvents fastZipEvents = new FastZipEvents();
-            //     fastZipEvents.CompletedFile = ZipFileComplete;
-            //     fastZipEvents.Progress = ZipFileProgress;
-            //     FastZip fastZip = new FastZip(fastZipEvents);
-            //     fastZip.ExtractZip(Path.Combine(Directory.GetCurrentDirectory(), tempFolderPath, teacherFileName), Path.Combine(Directory.GetCurrentDirectory(), "client"), null);
-            // }
-            // else
-            //     OpenTeacherClient();
-        }
-
-        private void ZipFileProgress(object sender, ProgressEventArgs args)
-        {
-            Debug.WriteLine("ZipFileProgress " + args.PercentComplete);
-        }
-
-        private async void ZipFileComplete(object sender, ScanEventArgs args)
-        {
-            Debug.WriteLine("ZipFileComplete");
-            await Task.Delay(3000);
+            UpdateServerFolder();
+            UpdateTeacherFolder();
             OpenTeacherClient();
         }
+
+        //private void ZipFileProgress(object sender, ProgressEventArgs args)
+        //{
+        //    Debug.WriteLine("ZipFileProgress " + args.PercentComplete);
+        //}
+
+        //private async void ZipFileComplete(object sender, ScanEventArgs args)
+        //{
+        //    Debug.WriteLine("ZipFileComplete");
+        //    await Task.Delay(3000);
+        //    OpenTeacherClient();
+        //}
 
         private void OpenTeacherClient()
         {
             Debug.WriteLine("OpenTeacherClient");
-            //try
-            //{
-            //    process process = new process();
-            //    processstartinfo startinfo = new processstartinfo();
-            //    startinfo.windowstyle = processwindowstyle.hidden;
-            //    startinfo.filename = "cmd.exe";
-            //    startinfo.arguments = "/c forever start server/index.js";
-            //    process.startinfo = startinfo;
-            //    process.start();
-            //}catch(exception ex){
-            //    debug.writeline(ex.tostring());
-            //}
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startinfo = new ProcessStartInfo();
+                startinfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startinfo.FileName = "cmd.exe";
+                startinfo.Arguments = "/c forever start server/index.js";
+                process.StartInfo = startinfo;
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
 
-            Process.Start(Path.Combine(Directory.GetCurrentDirectory(),"client","run.exe"));
+            Process.Start(Path.Combine(Directory.GetCurrentDirectory(), "client", "run.exe"));
             Application.Exit();
         }
 
@@ -154,8 +251,15 @@ namespace VRClassUpdater
                 JToken fileInfo= fileInfos[fileIndex];
                 string fileFullName = fileInfo["path"].ToString() + fileInfo["name"];
                 label1.Text = string.Format("下载中({0}/{1})", fileIndex + 1, fileInfos.Count);
-                webClient.DownloadFileAsync(new Uri(downloadRoot + fileInfos), tempFolderPath + fileInfos);
-                Debug.WriteLine("downloading ... " + downloadRoot + fileInfo["path"] + fileInfo["name"] + " " + tempFolderPath +  fileInfo["path"] + fileInfo["name"] );
+
+                
+                if (!Directory.Exists(tempFolderPath + fileInfo["path"]))
+                {
+                    Directory.CreateDirectory(tempFolderPath + fileInfo["path"]);
+                }
+
+                webClient.DownloadFileAsync(new Uri(downloadRoot + fileFullName), tempFolderPath + fileFullName);
+                Debug.WriteLine("downloading2 ... " + downloadRoot + fileInfo["path"] + fileInfo["name"] + " " + tempFolderPath +  fileInfo["path"] + fileInfo["name"] );
             }
             else
             {
@@ -169,6 +273,17 @@ namespace VRClassUpdater
             progressBar1.Value = e.ProgressPercentage;
         }
 
+        private JObject SaveVersion()
+        {
+            JObject version = new JObject();
+            version["version"] = versionNumber;
+            using (StreamWriter sw = new StreamWriter(localConfigFileName))
+            {
+                sw.Write(version.ToString());
+            }
+            return version;
+        }
+
         private async Task<JObject> GetLocalVersion()
         {
             String line;
@@ -178,17 +293,14 @@ namespace VRClassUpdater
                 {
                     line = await sr.ReadToEndAsync();
                 }
-                return new JObject(line);
+                return JObject.Parse(line);
             }
             catch (Exception ex)
             {
-                JObject version = new JObject();
-                version["version"] = "1.0.0";
-                using (StreamWriter sw = new StreamWriter(localConfigFileName))
-                {
-                    sw.Write(version.ToString());
-                }
-                return version;
+                Debug.WriteLine("GetLocalVersion err");
+                Debug.WriteLine(ex.ToString());
+                versionNumber = "1.0.0";
+                return SaveVersion();
             }
         }
 
